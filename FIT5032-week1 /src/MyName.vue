fab7043994043944f6227ef59e83629ffb6372f8
Danielzu12138 <2310496303@qq.com>
2026-06-30 22:28:27 +0800

    Remove submodule, re-add full fit5032-week1 normal folder

---
+++ FIT5032-week1 /src/MyName.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 展示你的姓名，替换成自己的名字 -->
+  <div class="name-box">
+    <h2>My Name: Zhihao Jin</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.name-box {
+  margin-top: 20px;
+  color: #2c3e50;
+  font-size: 18px;
+}
+</style>
