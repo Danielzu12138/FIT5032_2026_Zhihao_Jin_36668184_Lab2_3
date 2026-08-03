c2e3e21cd4afff23ca284dffcf40847997e1e5ad
Danielzu12138 <2310496303@qq.com>
2026-08-03 10:29:33 +0800

    Set up CI/CD deployment to GitHub Pages

--- jzjin-library/vite.config.js
+++ jzjin-library/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_2026_Zhihao_Jin_36668184_Lab2_3/'
+    : '/',
   plugins: [vue()]
 })
