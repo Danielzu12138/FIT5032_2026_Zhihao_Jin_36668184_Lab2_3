d1ae52cb735e55276c3a499bb8ca0efd208466a7
Danielzu12138 <2310496303@qq.com>
2026-08-03 21:44:23 +0800

    Fix base path for dual deployment

--- NoMash-Library/vite.config.js
+++ NoMash-Library/vite.config.js
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+/* global process */
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 👇 把这里改成了 '/'
-  base: '/', 
+  base: process.env.CF_PAGES
+    ? '/'
+    : process.env.NODE_ENV === 'production'
+      ? '/FIT5032_2026_Zhihao_Jin_36668184_Lab2_3/'
+      : '/',
   plugins: [
     vue(),
     vueDevTools(),
